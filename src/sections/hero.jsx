9bbbed3f948d967b1f932c66b29a50d43a7de8b2
rikaari <reganari48@gmail.com>
2025-12-30 18:59:13 +0530

    Update hero/mobile tweaks

--- src/sections/hero.jsx
+++ src/sections/hero.jsx
@@ -55,7 +55,7 @@ const hero = () => {
 
   return (
     <section id="hero" className='relative overflow-hidden'>
-      <div className='absolute top-0 left-0 z-10'>
+      <div className='absolute top-0 left-0 z-0'>
         <img src="./images/bg.png" alt="background" />
       </div>
 
